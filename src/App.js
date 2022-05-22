2843855405f463f9592cc9b18268fc492889773e
Tian Yao <yao.tian@avient.com>
2022-05-22 21:48:25 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component, Fragment } from 'react';
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+    <div className="container">
+
+      <div className="row">
+        <h1 className="mt-3">
+          Go Watch a Movie!
+        </h1>
+        <hr className="mb-3"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/movies">Movies</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/admin">Manage Catalogue</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="col-md-10">
+          <Switch>
+            <Route path="/movies">
+              <Movies />
+            </Route>
+            <Route path="/admin">
+              <Admin />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Movies() {
+  return <h2>Movies</h2>
+}
+
+function Admin() {
+  return <h2>Manage Catalogue</h2>
+}
