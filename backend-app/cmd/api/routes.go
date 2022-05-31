0e2a085dea54bc9f6a34b6da47f7a0c68b835759
Tian Yao <yao.tian@avient.com>
2022-05-31 15:54:08 +0800

    set up go backend with restful API

---
+++ backend-app/cmd/api/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+
+	return router
+}
