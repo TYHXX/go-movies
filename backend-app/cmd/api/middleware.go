676bbc016a8b447623ae4b489dbb0de47b71e8d7
Tian Yao <yao.tian@avient.com>
2022-06-01 11:12:50 +0800

    genres API

--- backend-app/cmd/api/middleware.go
+++ backend-app/cmd/api/middleware.go
@@ -3,9 +3,9 @@ package main
 import "net/http"
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		next.ServeHTTP(w, r)
-	})
+        next.ServeHTTP(w, r)
+    })
 }
--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
@@ -13,6 +14,8 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+
+	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
 	return app.enableCORS(router)
 }
